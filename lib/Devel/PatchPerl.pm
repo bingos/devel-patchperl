e632a026c63aedb78f6eb9670994e832e7ea6511
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2010-05-17 21:52:25 +0100

    Patch in.

---
+++ lib/Devel/PatchPerl.pm
@@ -0,0 +1,332 @@
+package Devel::PatchPerl;
+
+use strict;
+use warnings;
+use File::chdir;
+use File::Spec;
+use IO::File;
+use IPC::Cmd qw[can_run run];
+use vars qw[$VERSION];
+
+$VERSION = '0.02';
+
+my $patch_exe = can_run('patch');
+
+my @patch = (
+  {
+    perl => [
+              qr/^5\.00[01234]/,
+              qw/
+                5.005
+                5.005_01
+                5.005_02
+                5.005_03
+              /,
+            ],
+    subs => [
+              [ \&_patch_db, 1 ],
+            ],
+  },
+  {
+    perl => [
+            qw/
+                5.6.0
+                5.6.1
+                5.7.0
+                5.7.1
+                5.7.2
+                5.7.3
+                5.8.0
+            /,
+            ],
+    subs => [
+              [ \&_patch_db, 3 ],
+            ],
+  },
+  {
+    perl => [
+              qr/^5\.004_0[1234]$/,
+            ],
+    subs => [
+              [ \&_patch_doio ],
+            ],
+  },
+  {
+    perl => [
+              qw/
+                5.005
+                5.005_01
+                5.005_02
+              /,
+            ],
+    subs => [
+              [ \&_patch_sysv, old_format => 1 ],
+            ],
+  },
+  {
+    perl => [
+              qw/
+                5.005_03
+                5.005_04
+              /,
+              qr/^5\.6\.[0-2]$/,
+              qr/^5\.7\.[0-3]$/,
+              qr/^5\.8\.[0-8]$/,
+              qr/^5\.9\.[0-5]$/
+            ],
+    subs => [
+              [ \&_patch_sysv ],
+            ],
+  },
+  {
+    perl => [
+              qr/^5\.004_05$/,
+              qr/^5\.005(?:_0[1-4])?$/,
+              qr/^5\.6\.[01]$/,
+            ],
+    subs => [
+              [ \&_patch_configure ],
+              [ \&_patch_makedepend_lc ],
+            ],
+  },
+  {
+    perl => [
+              '5.8.0',
+            ],
+    subs => [
+              [ \&_patch_makedepend_lc ],
+            ],
+  },
+);
+
+sub patch_source {
+  my $vers = shift;
+  $vers = shift if $vers->isa(__PACKAGE__);
+  my $source = shift || '.';
+  $source = File::Spec->rel2abs($source);
+  {
+    local $CWD = $source;
+    for my $p ( grep { _is( $_->{perl}, $vers ) } @patch ) {
+       for my $s (@{$p->{subs}}) {
+         my($sub, @args) = @$s;
+         $sub->(@args);
+       }
+    }
+  }
+}
+
+sub _is
+{
+  my($s1, $s2) = @_;
+
+  defined $s1 != defined $s2 and return 0;
+
+  ref $s2 and ($s1, $s2) = ($s2, $s1);
+
+  if (ref $s1) {
+    if (ref $s1 eq 'ARRAY') {
+      is($_, $s2) and return 1 for @$s1;
+      return 0;
+    }
+    return $s2 =~ $s1;
+  }
+
+  return $s1 eq $s2;
+}
+
+sub _patch_db
+{
+  my $ver = shift;
+  print "patching ext/DB_File/DB_File.xs\n";
+  _run_or_die($^X, '-pi.bak', '-e', "s/<db.h>/<db$ver\\/db.h>/", 'ext/DB_File/DB_File.xs');
+  unlink 'ext/DB_File/DB_File.xs.bak' if -e 'ext/DB_File/DB_File.xs.bak';
+}
+
+sub _patch_doio
+{
+  _patch(<<'END');
+--- doio.c.org  2004-06-07 23:14:45.000000000 +0200
++++ doio.c  2003-11-04 08:03:03.000000000 +0100
+@@ -75,6 +75,16 @@
+ #  endif
+ #endif
+
++#if _SEM_SEMUN_UNDEFINED
++union semun
++{
++  int val;
++  struct semid_ds *buf;
++  unsigned short int *array;
++  struct seminfo *__buf;
++};
++#endif
++
+ bool
+ do_open(gv,name,len,as_raw,rawmode,rawperm,supplied_fp)
+ GV *gv;
+END
+}
+
+sub _patch_sysv
+{
+  my %opt = @_;
+
+  # check if patching is required
+  return if $^O ne 'linux' or -f '/usr/include/asm/page.h';
+
+  if ($opt{old_format}) {
+    _patch(<<'END');
+--- ext/IPC/SysV/SysV.xs.org  1998-07-20 10:20:07.000000000 +0200
++++ ext/IPC/SysV/SysV.xs  2007-08-12 10:51:06.000000000 +0200
+@@ -3,9 +3,6 @@
+ #include "XSUB.h"
+ 
+ #include <sys/types.h>
+-#ifdef __linux__
+-#include <asm/page.h>
+-#endif
+ #if defined(HAS_MSG) || defined(HAS_SEM) || defined(HAS_SHM)
+ #include <sys/ipc.h>
+ #ifdef HAS_MSG
+END
+  }
+  else {
+    _patch(<<'END');
+--- ext/IPC/SysV/SysV.xs.org  2007-08-11 00:12:46.000000000 +0200
++++ ext/IPC/SysV/SysV.xs  2007-08-11 00:10:51.000000000 +0200
+@@ -3,9 +3,6 @@
+ #include "XSUB.h"
+ 
+ #include <sys/types.h>
+-#ifdef __linux__
+-#   include <asm/page.h>
+-#endif
+ #if defined(HAS_MSG) || defined(HAS_SEM) || defined(HAS_SHM)
+ #ifndef HAS_SEM
+ #   include <sys/ipc.h>
+END
+  }
+}
+
+sub _patch_configure
+{
+  _patch(<<'END');
+--- Configure
++++ Configure
+@@ -3380,6 +3380,18 @@
+ test "X$gfpthkeep" != Xy && gfpth=""
+ EOSC
+ 
++# gcc 3.1 complains about adding -Idirectories that it already knows about,
++# so we will take those off from locincpth.
++case "$gccversion" in
++3*)
++    echo "main(){}">try.c
++    for incdir in `$cc -v -c try.c 2>&1 | \
++       sed '1,/^#include <\.\.\.>/d;/^End of search list/,$d;s/^ //'` ; do
++       locincpth=`echo $locincpth | sed s!$incdir!!`
++    done
++    $rm -f try try.*
++esac
++
+ : What should the include directory be ?
+ echo " "
+ $echo $n "Hmm...  $c"
+END
+}
+
+sub _patch_makedepend_lc
+{
+  patch(<<'END');
+--- makedepend.SH
++++ makedepend.SH
+@@ -58,6 +58,10 @@ case $PERL_CONFIG_SH in
+       ;;
+ esac
+ 
++# Avoid localized gcc/cc messages
++LC_ALL=C
++export LC_ALL
++
+ # We need .. when we are in the x2p directory if we are using the
+ # cppstdin wrapper script.
+ # Put .. and . first so that we pick up the present cppstdin, not
+END
+}
+
+sub _patch
+{
+  my($patch) = @_;
+  print "patching $_\n" for $patch =~ /^\+{3}\s+(\S+)/gm;
+  my $diff = 'tmp.diff';
+  _write_or_die($diff, $patch);
+  _run_or_die($patch_exe, '-s', '-p0', "<$diff");
+  unlink $diff or die "unlink $diff: $!\n";
+}
+
+sub _write_or_die
+{
+  my($file, $data) = @_;
+  my $fh = IO::File->new(">$file") or die "$file: $!\n";
+  $fh->print($data);
+}
+
+sub _run_or_die
+{
+  # print "[running @_]\n";
+  die unless scalar run( command => [ @_ ], verbose => 1 );
+}
+
+qq[patchin'];
+
+__END__
+
+=head1 NAME
+
+Devel::PatchPerl - Patch perl source a la Devel::PPort's buildperl.pl
+
+=head1 SYNOPSIS
+
+  use strict;
+  use warnings;
+
+  use Devel::PatchPerl;
+
+  Devel::PatchPerl->patch_source( '5.6.1', '/path/to/untarred/perl/source/perl-5.6.1' );
+
+=head1 DESCRIPTION
+
+Devel::PatchPerl is a modularisation of the patching code contained in L<Devel::PPort>'s 
+C<buildperl.pl>.
+
+It does not build perls, it merely provides an interface to the source patching 
+functionality.
+
+=head1 FUNCTION
+
+=over
+
+=item C<patch_source>
+
+Takes two parameters, a C<perl> version and the path to unwrapped perl source for that version.
+It dies on any errors.
+
+=back
+
+=head1 AUTHOR
+
+Chris C<BinGOs> Williams <chris@bingosnet.co.uk>
+
+Marcus Holland-Moritz 
+
+=head1 LICENSE
+
+Copyright E<copy> Chris Williams and Marcus Holland-Moritz
+
+This module may be used, modified, and distributed under the same terms as Perl itself. Please see the license that came with your Perl distribution for details.
+
+=head1 SEE ALSO
+
+L<Devel::PPPort>
+
+=cut
---
+++ t/00_compile.t
@@ -0,0 +1,4 @@
+use strict;
+use warnings;
+use Test::More qw[no_plan];
+use_ok('Devel::PatchPerl');
