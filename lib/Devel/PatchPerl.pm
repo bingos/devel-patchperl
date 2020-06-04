0ab4bf0d8b02af79e1afed7fac9e91b75f1c1fda
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2020-06-04 11:20:07 +0100

    Resolve patching issues with v5.22.x and v5.24.x
    https://github.com/bingos/devel-patchperl/issues/44
    
    Configure changes had been backported to the v5.22 and v5.24
    maint releases, so what worked for .0 releases didn't work
    for later maint releases.
    
    I have adjusted and tested the patch application criteria
    for all v5.22.x and v5.24.x releases.

--- lib/Devel/PatchPerl.pm
+++ lib/Devel/PatchPerl.pm
@@ -10168,7 +10168,7 @@ Y3BwZmxhZ3M9IiRjcHBmbGFncyAtRF9fR05VQ19fIgorMS4qKSBjcHBmbGFncz0iJGNwcGZsYWdz
 IC1EX19HTlVDX18iCiBlc2FjCiBjYXNlICIkbWlwc190eXBlIiBpbgogJycpOzsK
 CONFGCC10520
   }
-  if ( $num < 5.023005 ) {
+  if ( $num < 5.023005 and ! ( $num >= 5.022002 and $num < 5.023000 ) ) {
     return _patch_b64(<<'CONFGCC10522');
 LS0tIENvbmZpZ3VyZQorKysgQ29uZmlndXJlCkBAIC00NjU3LDcgKzQ2NTcsNyBAQCBlbHNlCiBm
 aQogJHJtIC1mIHRyeSB0cnkuKgogY2FzZSAiJGdjY3ZlcnNpb24iIGluCi0xKikgY3BwPWAuL2xv
@@ -10204,7 +10204,9 @@ IHNlZW0gdG8gaGF2ZSAtRF9GT1JUSUZZX1NPVVJDRSBhbHJlYWR5LCBub3QgYWRkaW5nIGl0LiIg
 PiY0Cg==
 CONFGCC10522
   }
-  if ( $num <= 5.026000 or ( $num >= 5.027000 and $num < 5.027003 ) ) {
+  if ( ( $num <= 5.026000 or ( $num >= 5.027000 and $num < 5.027003 ) )
+       and ! ( $num >= 5.024003 and $num < 5.025000  )
+  ) {
     return _patch_b64(<<'CONFGCC10526');
 LS0tIENvbmZpZ3VyZQorKysgQ29uZmlndXJlCkBAIC00NzAzLDcgKzQ3MDMsNyBAQCBlbHNlCiBm
 aQogJHJtIC1mIHRyeSB0cnkuKgogY2FzZSAiJGdjY3ZlcnNpb24iIGluCi0xKikgY3BwPWAuL2xv
