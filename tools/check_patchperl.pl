fa458343e6ae7f70412c466eb604633db44bf24b
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2020-06-09 16:50:17 +0100

    Finesse the check_patchperl script
    Suppress the git output and only show patchperl output
    if there is a problem with patching

--- tools/check_patchperl.pl
+++ tools/check_patchperl.pl
@@ -2,9 +2,10 @@ use 5.012;
 use Perl::Version;
 use Git::Repository;
 use ExtUtils::MakeMaker;
+use Capture::Tiny qw[capture_merged];
 
 Git::Repository->run( clone => 'https://github.com/Perl/perl5.git', 'perl.git' ) unless -d 'perl.git';
-my $r = Git::Repository->new( work_tree => 'perl.git' );
+my $r = Git::Repository->new( work_tree => 'perl.git', { quiet => 1 } );
 my @tags = grep { m!^(perl-|v)5\.! && !m!5\.00! && !m!-RC\d+$! && !m!^\Qv5.17.7.0\E$! && !m!^perl-5\.10\.! } $r->run( tag => '-l' );
 {
   my @ptags = grep { m!^perl! } @tags;
@@ -13,9 +14,16 @@ my @tags = grep { m!^(perl-|v)5\.! && !m!5\.00! && !m!-RC\d+$! && !m!^\Qv5.17.7.
 }
 
 foreach my $tag ( @tags ) {
+  say "Checking $tag ...";
   $r->run( checkout => $tag );
-  my $value;
-  system( $^X, '-Ilib', 'bin/patchperl', 'perl.git' ) == 0 or $value = prompt("Continue [Y/n]?","y");
+  my $ret; my $value;
+  my $merged = capture_merged {
+    $ret = system( $^X, '-Ilib', 'bin/patchperl', 'perl.git' );
+  };
+  unless ( $ret == 0 ) {
+    print $merged;
+    $value = prompt("Continue [Y/n]?","y") unless $ret == 0;
+  }
   last if $value =~ m!^n!i;
   $r->run( reset => '--hard' );
   $r->run( clean => '-dxf' );
